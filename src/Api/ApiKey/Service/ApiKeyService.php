aae1dab0642f6fd4b92a081ef3b89f3010b83dec
luka-lta <luka@luka-lta.dev>
2025-03-31 22:13:26 +0200

    #67 Review changes

--- src/Api/ApiKey/Service/ApiKeyService.php
+++ src/Api/ApiKey/Service/ApiKeyService.php
@@ -65,6 +65,7 @@ class ApiKeyService
         if ($apiKeys->count() === 0) {
             return ApiResult::from(
                 JsonResult::from('No API keys found', ['apiKeys' => []]),
+                StatusCodeInterface::STATUS_NOT_FOUND
             );
         }
 
